00f2f53691c5a582a76cf8b0dc6245fc4543ac72
Ed Efimov <edefimov03@gmail.com>
2025-03-09 15:47:31 +0000

    First commit

---
+++ tasks/cache.py
@@ -0,0 +1,15 @@
+cache = {}
+
+def get_page(url):
+    if url in cache:
+        return cache[url]
+    else:
+        data = get_data_from_server(url)
+        cache[url] = data
+        return data
+    
+def get_data_from_server(url):
+    return f"Стартовая страница {url}" 
+
+print(get_page("google.com"))
+print(get_page("google.com"))
