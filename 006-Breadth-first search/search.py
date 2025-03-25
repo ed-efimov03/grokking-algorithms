936f83e6f5ec6a0c3a1ab401b646cfbbaae4c050
Ed Efimov <edefimov03@gmail.com>
2025-03-25 17:46:47 +0000

    rels #996. Add gitignore, change readme, change file folder

---
+++ 006-Breadth-first search/search.py
@@ -0,0 +1,31 @@
+if __name__ == "__main__":
+    from collections import deque
+
+    def search(name):
+        search_queue = deque()
+        search_queue += graph[name]
+        searched = set()
+        while search_queue:
+            person = search_queue.popleft()
+            if not person in searched:
+                if person_is_seller(person):
+                    print(person + " продавец манго!")
+                else:
+                    search_queue += graph[person]
+                    searched.add(person)
+        return False
+
+    def person_is_seller(seller):
+        return seller[-1] == "m"
+
+    graph = {}
+    graph["вы"] = ["alice", "bob", "claire"]
+    graph["bob"] = ["anuj", "peggy"]
+    graph["alice"] = ["peggy"]
+    graph["claire"] = ["thom", "jonny"]
+    graph["anuj"] = []
+    graph["peggy"] = []
+    graph["thom"] = []
+    graph["jonny"] = []
+
+    search("вы")
