bb6b05ebe114b75dd6cd6406c084e4af42e308b7
Ed Efimov <edefimov03@gmail.com>
2025-03-25 17:24:07 +0000

    rels #994. Add if name=main

--- 004-Quick sort/quick_sort.py
+++ 004-Quick sort/quick_sort.py
@@ -1,23 +1,24 @@
-def quick_sort(arr):
-    if len(arr) <= 1 :
-        return arr
-    else:
-        mid = arr[len(arr)//2]
+if __name__ == "__main__":
+    def quick_sort(arr):
+        if len(arr) <= 1 :
+            return arr
+        else:
+            mid = arr[len(arr)//2]
 
-        l = list()
-        r = list()
-        e = list()
+            l = list()
+            r = list()
+            e = list()
 
-        for n in arr:
-            if n < mid:
-                l.append(n)
-            elif n > mid:
-                r.append(n)
-            else:
-                e.append(n)
+            for n in arr:
+                if n < mid:
+                    l.append(n)
+                elif n > mid:
+                    r.append(n)
+                else:
+                    e.append(n)
 
-        return quick_sort(l) + e + quick_sort(r)
+            return quick_sort(l) + e + quick_sort(r)
 
-print(quick_sort([4, 1, 6, 3, 2, 7, 8]))
+    print(quick_sort([4, 1, 6, 3, 2, 7, 8]))
 
             
--- 004-Quick sort/sum.py
+++ 004-Quick sort/sum.py
@@ -1,6 +1,7 @@
-def sum_numbers(arr):
-    if len(arr) == 0:
-        return 0
-    return arr[0] + sum_numbers(arr[1:])
+if __name__ == "__main__":
+    def sum_numbers(arr):
+        if len(arr) == 0:
+            return 0
+        return arr[0] + sum_numbers(arr[1:])
 
-print(sum_numbers([1, 2, 3, 4]))
+    print(sum_numbers([1, 2, 3, 4]))
