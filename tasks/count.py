910dbd34c32e656515957d9f759600be6ed9eef6
Ed Efimov <edefimov03@gmail.com>
2025-03-25 17:04:00 +0000

    Delete remote folder tasks

--- tasks/count.py
+++
@@ -1,6 +0,0 @@
-def count_numbers(arr):
-    if len(arr) == 0:
-        return 0
-    return 1 + count_numbers(arr[1:])
-
-print(count_numbers([1, 2, 3, 4]))
--- tasks/sum.py
+++
@@ -1,6 +0,0 @@
-def sum_numbers(arr):
-    if len(arr) == 0:
-        return 0
-    return arr[0] + sum_numbers(arr[1:])
-
-print(sum_numbers([1, 2, 3, 4]))
