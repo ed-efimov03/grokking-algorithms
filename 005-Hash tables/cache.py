f89275d4247e0c119d7cdd83187a88f14f50d35f
Ed Efimov <edefimov03@gmail.com>
2025-03-25 17:40:24 +0000

    rels #995. Add if name=main

--- 005-Hash tables/cache.py
+++ 005-Hash tables/cache.py
@@ -1,15 +1,16 @@
-cache = {}
+if __name__ == "__main__":
+    cache = {}
 
-def get_page(url):
-    if url in cache:
-        return cache[url]
-    else:
-        data = get_data_from_server(url)
-        cache[url] = data
-        return data
-    
-def get_data_from_server(url):
-    return f"Стартовая страница {url}" 
+    def get_page(url):
+        if url in cache:
+            return cache[url]
+        else:
+            data = get_data_from_server(url)
+            cache[url] = data
+            return data
+        
+    def get_data_from_server(url):
+        return f"Стартовая страница {url}" 
 
-print(get_page("google.com"))
-print(get_page("google.com"))
+    print(get_page("google.com"))
+    print(get_page("google.com"))
--- 005-Hash tables/voters.py
+++ 005-Hash tables/voters.py
@@ -1,12 +1,13 @@
-voted = {}
+if __name__ == "__main__":
+    voted = {}
 
-def check_voter(name):
-    if name in voted:
-        print("Выгнать его!")
-    else:
-        voted[name] = True
-        print("Допустить к голосованию")
+    def check_voter(name):
+        if name in voted:
+            print("Выгнать его!")
+        else:
+            voted[name] = True
+            print("Допустить к голосованию")
 
-check_voter("Ed")
-check_voter("John")
-check_voter("Ed")
+    check_voter("Ed")
+    check_voter("John")
+    check_voter("Ed")
